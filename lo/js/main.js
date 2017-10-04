b061e6aa6c16915d115619f29561ae7cc30c60e8
Lamina Olawale <candidreloaded@yahoo.com>
2017-10-03 20:59:55 -0400

    Portfolio filter by categories

--- lo/js/main.js
+++ lo/js/main.js
@@ -1,15 +1,5 @@
 
-$(document).ready(function(){ 
-    
-    // Prevent background scrolling
-    $('.burg-toggle').click(function(e){
-        $(document.body).addClass('fixedposition');
-    });
-        
-    $('.close-button').click(function(e){
-        $(document.body).removeClass('fixedposition');
-    });
-    
+$(document).ready(function(){  
     //Testimonial 
     $("#testimonial-slider").owlCarousel({
         items:1,
@@ -26,9 +16,37 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){ 
+     // Prevent background scrolling
+     $('.burg-toggle').click(function(e){
+        $(document.body).addClass('fixedposition');
+    });
+        
+    $('.close-button').click(function(e){
+        $(document.body).removeClass('fixedposition');
+    });
+
     //Portfolio categories
     $('.port_cat').on('click', function(){
        $('.port_cat').removeClass('active');
             $(this).addClass('active');
     });
+
+    // Porfolio filter
+    $(".port_cat").click(function(){
+        var value = $(this).attr('data-filter');
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+        }
+    });
 });
+
+
+
+
+
